47236fa3fb2b3a92829d942e86e205f9485a1fc5
Vincent Jacquelin <vjacquelinpro@gmail.com>
2025-03-29 10:04:12 +0100

    responsive optimisation

--- src/components/Filter/AutoComplete.jsx
+++ src/components/Filter/AutoComplete.jsx
@@ -1,10 +1,22 @@
+import { useState } from "react";
+
 const AutoComplete = ({ data, filters, setFilters }) => {
+  const [displayAutoCom, setDisplayAutoComp] = useState(true);
+  console.log(filters);
+
   let autoCompleteCount = 0;
   return (
-    <div className="all-auto-complete">
-      {data.map((autocomplete) => {
-        while (autoCompleteCount < 10) {
-          autoCompleteCount++;
+    displayAutoCom && (
+      <div
+        className={
+          filters.name
+            ? "all-auto-complete display-auto-comp"
+            : "all-auto-complete"
+        }
+      >
+        {data.map((autocomplete) => {
+          // while (autoCompleteCount < 10) {
+          //   autoCompleteCount++;
           return (
             <div
               className="auto-complete"
@@ -15,14 +27,16 @@ const AutoComplete = ({ data, filters, setFilters }) => {
                     ? autocomplete.title
                     : autocomplete.name,
                 });
+                setDisplayAutoComp(!displayAutoCom);
               }}
             >
               {autocomplete.title ? autocomplete.title : autocomplete.name}
             </div>
           );
-        }
-      })}
-    </div>
+          // }
+        })}
+      </div>
+    )
   );
 };
 export default AutoComplete;
